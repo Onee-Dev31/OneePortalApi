5dd8753b24d660a1fc0cb724888c61f16cf29360
Flukelnw <flukelnw1142@gmail.com>
2025-11-12 11:39:02 +0700

    fix

---
+++ OnePortal-Api/Model/Countries.cs
@@ -0,0 +1,8 @@
+﻿namespace OnePortal_Api.Model
+{
+    public class Countries
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
---
+++ OnePortal-Api/Model/SupplierBank.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnePortal_Api.Model
+{
+    public class SupplierBank
+    {
+        [Key]
+        public int SupbankId { get; set; }
+        public int SupplierId { get; set; }
+        public  string? NameBank { get; set; }
+        public  string? Branch { get; set; }
+        public  string? AccountNum { get; set; }
+        public  string? AccountName { get; set; }
+        public  string? SupplierGroup { get; set; }
+        public string? Company { get; set; }
+    }
+}
