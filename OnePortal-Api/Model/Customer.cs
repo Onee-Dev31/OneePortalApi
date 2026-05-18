0f27c753e12b2727527abe55ff549b3aad45dcfb
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2026-05-18 16:27:04 +0700

    Update Customer.cs

--- OnePortal-Api/Model/Customer.cs
+++ OnePortal-Api/Model/Customer.cs
@@ -8,7 +8,7 @@ namespace OnePortal_Api.Model
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string TaxId { get; set; }
-        public required string AddressSup { get; set; }
+        public required string? AddressSup { get; set; }
         public required string District { get; set; }
         public required string Subdistrict { get; set; }
         public required string Province { get; set; }
